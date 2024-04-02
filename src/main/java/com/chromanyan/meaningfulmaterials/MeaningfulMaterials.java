e45f3230980c0d2876079e988adc48ff257711b8
ChromaNyan <nyancatz593@gmail.com>
2024-04-02 14:50:00 -0500

    feat: initial commit

---
+++ src/main/java/com/chromanyan/meaningfulmaterials/MeaningfulMaterials.java
@@ -0,0 +1,42 @@
+package com.chromanyan.meaningfulmaterials;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(MeaningfulMaterials.MODID)
+public class MeaningfulMaterials {
+
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "meaningfulmaterials";
+    // Directly reference a slf4j logger
+    @SuppressWarnings("unused")
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public MeaningfulMaterials() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        // Some common setup code
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // Do something when the server starts
+    }
+}
