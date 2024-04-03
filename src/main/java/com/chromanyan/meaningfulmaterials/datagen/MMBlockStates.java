4fc6ac7bc839293e5dd6ef4eb9d00522d1808fce
ChromaNyan <nyancatz593@gmail.com>
2024-04-02 22:42:32 -0500

    refactor: unused stuff

--- src/main/java/com/chromanyan/meaningfulmaterials/datagen/MMBlockStates.java
+++ src/main/java/com/chromanyan/meaningfulmaterials/datagen/MMBlockStates.java
@@ -2,22 +2,14 @@ package com.chromanyan.meaningfulmaterials.datagen;
 
 import com.chromanyan.meaningfulmaterials.MeaningfulMaterials;
 import com.chromanyan.meaningfulmaterials.init.MMBlocks;
-import net.minecraft.core.Registry;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.registries.RegistryObject;
 
 public class MMBlockStates extends BlockStateProvider {
     public MMBlockStates(DataGenerator generator, ExistingFileHelper existingFileHelper) {
         super(generator, MeaningfulMaterials.MODID, existingFileHelper);
-    }
-
-    public ModelFile cubeAll(String name) {
-        return models().cubeAll(name, modLoc("block/" + name));
     }
 
     @Override
