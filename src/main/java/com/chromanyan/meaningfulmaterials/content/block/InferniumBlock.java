25b2513d3283343ae61423b3a5937e95715163a4
ChromaNyan <nyancatz593@gmail.com>
2024-04-30 17:53:59 -0500

    feat: infernium blocks now cause hot floor damage

---
+++ src/main/java/com/chromanyan/meaningfulmaterials/content/block/InferniumBlock.java
@@ -0,0 +1,24 @@
+package com.chromanyan.meaningfulmaterials.content.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+
+public class InferniumBlock extends Block {
+
+    public InferniumBlock(Properties pProperties) {
+        super(pProperties);
+    }
+
+    public void stepOn(@NotNull Level pLevel, @NotNull BlockPos pPos, @NotNull BlockState pState, Entity pEntity) {
+        if (!pEntity.isSteppingCarefully() && pEntity instanceof LivingEntity livingEntity && !EnchantmentHelper.hasFrostWalker(livingEntity)) {
+            pEntity.hurt(DamageSource.HOT_FLOOR, 1.0F);
+        }
+    }
+}
--- src/main/java/com/chromanyan/meaningfulmaterials/init/MMBlocks.java
+++ src/main/java/com/chromanyan/meaningfulmaterials/init/MMBlocks.java
@@ -2,6 +2,7 @@ package com.chromanyan.meaningfulmaterials.init;
 
 import com.chromanyan.meaningfulmaterials.MeaningfulMaterials;
 import com.chromanyan.meaningfulmaterials.content.block.CosmicLanternBlock;
+import com.chromanyan.meaningfulmaterials.content.block.InferniumBlock;
 import com.chromanyan.meaningfulmaterials.content.block.InferniumOreBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
@@ -28,7 +29,7 @@ public class MMBlocks {
     );
 
     public static final RegistryObject<Block> INFERNIUM_BLOCK = BLOCKS_REGISTRY.register("infernium_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_ORANGE)
+            () -> new InferniumBlock(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_ORANGE)
                     .requiresCorrectToolForDrops()
                     .strength(5.0F, 6.0F)
                     .lightLevel(value -> 7)
@@ -37,7 +38,7 @@ public class MMBlocks {
     public static final RegistryObject<Block> INFERNIUM_ORE = BLOCKS_REGISTRY.register("infernium_ore", InferniumOreBlock::new);
 
     public static final RegistryObject<Block> RAW_INFERNIUM_BLOCK = BLOCKS_REGISTRY.register("raw_infernium_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_ORANGE)
+            () -> new InferniumBlock(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_ORANGE)
                     .requiresCorrectToolForDrops()
                     .strength(5.0F, 6.0F)
                     .lightLevel(value -> 5))
