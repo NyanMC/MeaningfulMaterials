718960ba62be0ad7169ad12ac4014afcf8c9b12c
ChromaNyan <nyancatz593@gmail.com>
2024-05-18 20:37:44 -0500

    fix: MMItems

--- src/main/java/com/chromanyan/meaningfulmaterials/MeaningfulMaterials.java
+++ src/main/java/com/chromanyan/meaningfulmaterials/MeaningfulMaterials.java
@@ -11,9 +11,11 @@ import com.chromanyan.meaningfulmaterials.event.MMEvents;
 import com.chromanyan.meaningfulmaterials.init.*;
 import com.mojang.logging.LogUtils;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -41,6 +43,7 @@ public class MeaningfulMaterials {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
         modEventBus.addListener(this::gatherData);
+        modEventBus.addListener(this::addCreative);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
@@ -49,6 +52,39 @@ public class MeaningfulMaterials {
     private void commonSetup(final FMLCommonSetupEvent event) {
         MinecraftForge.EVENT_BUS.register(new MMEvents());
         MMDispenserBehaviors.registerBehaviors();
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey().equals(CreativeModeTabs.INGREDIENTS)) {
+            event.accept(MMItems.COSMITE.get());
+            event.accept(MMItems.INFERNIUM_INGOT.get());
+            event.accept(MMItems.RAW_INFERNIUM.get());
+            event.accept(MMItems.INFERNIUM_DUST.get());
+        }
+
+        if (event.getTabKey().equals(CreativeModeTabs.COMBAT)) {
+            event.accept(MMItems.COSMITE_BOOTS.get());
+            event.accept(MMItems.INFERNIUM_SWORD.get());
+        }
+
+        if (event.getTabKey().equals(CreativeModeTabs.TOOLS_AND_UTILITIES)) {
+            event.accept(MMItems.INFERNIUM_SHOVEL.get());
+            event.accept(MMItems.INFERNIUM_PICKAXE.get());
+            event.accept(MMItems.INFERNIUM_AXE.get());
+            event.accept(MMItems.INFERNIUM_HOE.get());
+        }
+
+        if (event.getTabKey().equals(CreativeModeTabs.BUILDING_BLOCKS)) {
+            event.accept(MMItems.COSMITE_BLOCK_ITEM.get());
+            event.accept(MMItems.INFERNIUM_BLOCK_ITEM.get());
+            event.accept(MMItems.COSMIC_LANTERN_ITEM.get());
+        }
+
+        if (event.getTabKey().equals(CreativeModeTabs.NATURAL_BLOCKS)) {
+            event.accept(MMItems.COSMITE_ORE_ITEM.get());
+            event.accept(MMItems.INFERNIUM_ORE_ITEM.get());
+            event.accept(MMItems.RAW_INFERNIUM_BLOCK_ITEM.get());
+        }
     }
 
     public void gatherData(GatherDataEvent event) {
--- src/main/java/com/chromanyan/meaningfulmaterials/init/MMItems.java
+++ src/main/java/com/chromanyan/meaningfulmaterials/init/MMItems.java
@@ -16,54 +16,54 @@ public class MMItems {
     public static final DeferredRegister<Item> ITEMS_REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MeaningfulMaterials.MODID);
 
     public static final RegistryObject<Item> COSMITE = ITEMS_REGISTRY.register("cosmite",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+            () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> INFERNIUM_INGOT = ITEMS_REGISTRY.register("infernium_ingot",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS).fireResistant()));
+            () -> new Item(new Item.Properties().fireResistant()));
     public static final RegistryObject<Item> RAW_INFERNIUM = ITEMS_REGISTRY.register("raw_infernium",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS).fireResistant()));
+            () -> new Item(new Item.Properties().fireResistant()));
     public static final RegistryObject<Item> INFERNIUM_DUST = ITEMS_REGISTRY.register("infernium_dust",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS).fireResistant()));
+            () -> new Item(new Item.Properties().fireResistant()));
 
     public static final RegistryObject<Item> COSMIC_ARROW = ITEMS_REGISTRY.register("cosmic_arrow", CosmicArrowItem::new);
 
     public static final RegistryObject<Item> COSMITE_BOOTS = ITEMS_REGISTRY.register("cosmite_boots",
-            () -> new CosmiteArmorItem(EquipmentSlot.FEET, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+            () -> new CosmiteArmorItem(EquipmentSlot.FEET, new Item.Properties()));
 
     public static final RegistryObject<Item> INFERNIUM_LIGHTER = ITEMS_REGISTRY.register("infernium_lighter", InferniumLighterItem::new);
 
     public static final RegistryObject<Item> INFERNIUM_SWORD = ITEMS_REGISTRY.register("infernium_sword",
             () -> new SwordItem(MMToolMaterials.INFERNIUM, 3, -2.4f,
-                    new Item.Properties().tab(CreativeModeTab.TAB_COMBAT).fireResistant()));
+                    new Item.Properties().fireResistant()));
 
     public static final RegistryObject<Item> INFERNIUM_SHOVEL = ITEMS_REGISTRY.register("infernium_shovel",
             () -> new ShovelItem(MMToolMaterials.INFERNIUM, 1.5f, -3.0f,
-                    new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).fireResistant()));
+                    new Item.Properties().fireResistant()));
 
     public static final RegistryObject<Item> INFERNIUM_PICKAXE = ITEMS_REGISTRY.register("infernium_pickaxe",
             () -> new PickaxeItem(MMToolMaterials.INFERNIUM, 1, -2.8f,
-                    new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).fireResistant()));
+                    new Item.Properties().fireResistant()));
 
     public static final RegistryObject<Item> INFERNIUM_AXE = ITEMS_REGISTRY.register("infernium_axe",
             () -> new AxeItem(MMToolMaterials.INFERNIUM, 6.0f, -3.0f,
-                    new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).fireResistant()));
+                    new Item.Properties().fireResistant()));
 
     public static final RegistryObject<Item> INFERNIUM_HOE = ITEMS_REGISTRY.register("infernium_hoe",
             () -> new HoeItem(MMToolMaterials.INFERNIUM, -2, 0.0f,
-                    new Item.Properties().tab(CreativeModeTab.TAB_TOOLS).fireResistant()));
+                    new Item.Properties().fireResistant()));
 
     public static final RegistryObject<BlockItem> COSMITE_BLOCK_ITEM = ITEMS_REGISTRY.register("cosmite_block",
-            () -> new BlockItem(MMBlocks.COSMITE_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
+            () -> new BlockItem(MMBlocks.COSMITE_BLOCK.get(), new Item.Properties()));
     public static final RegistryObject<BlockItem> COSMITE_ORE_ITEM = ITEMS_REGISTRY.register("cosmite_ore",
-            () -> new BlockItem(MMBlocks.COSMITE_ORE.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
+            () -> new BlockItem(MMBlocks.COSMITE_ORE.get(), new Item.Properties()));
 
     public static final RegistryObject<BlockItem> INFERNIUM_BLOCK_ITEM = ITEMS_REGISTRY.register("infernium_block",
-            () -> new BlockItem(MMBlocks.INFERNIUM_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS).fireResistant()));
+            () -> new BlockItem(MMBlocks.INFERNIUM_BLOCK.get(), new Item.Properties().fireResistant()));
     public static final RegistryObject<BlockItem> INFERNIUM_ORE_ITEM = ITEMS_REGISTRY.register("infernium_ore",
-            () -> new BlockItem(MMBlocks.INFERNIUM_ORE.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS).fireResistant()));
+            () -> new BlockItem(MMBlocks.INFERNIUM_ORE.get(), new Item.Properties().fireResistant()));
     public static final RegistryObject<BlockItem> RAW_INFERNIUM_BLOCK_ITEM = ITEMS_REGISTRY.register("raw_infernium_block",
-            () -> new BlockItem(MMBlocks.RAW_INFERNIUM_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS).fireResistant()));
+            () -> new BlockItem(MMBlocks.RAW_INFERNIUM_BLOCK.get(), new Item.Properties().fireResistant()));
 
     public static final RegistryObject<BlockItem> COSMIC_LANTERN_ITEM = ITEMS_REGISTRY.register("cosmic_lantern",
-            () -> new SimpleTooltipBlockItem(MMBlocks.COSMIC_LANTERN.get(), new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS), "tooltip.meaningfulmaterials.cosmic_lantern"));
+            () -> new SimpleTooltipBlockItem(MMBlocks.COSMIC_LANTERN.get(), new Item.Properties(), "tooltip.meaningfulmaterials.cosmic_lantern"));
 }
